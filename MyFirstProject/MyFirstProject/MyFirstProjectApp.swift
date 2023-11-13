bdf2e4e859bff5487d4fbee7e32f2fcc97003cf9
Viktoriia_Dill <Viktoriia_Dill>
2023-11-13 23:14:28 +0100

    hgukhli

---
+++ MyFirstProject/MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Пользователь on 13.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
